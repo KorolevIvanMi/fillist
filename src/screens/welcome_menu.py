96612f6e238585156c632ba08808bfee970c09b3
ivankorolev <ikorolev368@gmail.com>
2025-12-19 19:53:34 +0300

    add welcome menu design and more btns :wrench:

--- src/screens/welcome_menu.py
+++ src/screens/welcome_menu.py
@@ -2,12 +2,15 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.app import App
-
+from utils.helpers import * 
 class WelcomeMenu(FloatLayout):
     main_menu_btn = ObjectProperty()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        self.normal_prof_res = get_resource_path('resources/images/buttons/Profile_btn.png')
+        self.down_prof_res = get_resource_path('resources/images/buttons/Profile_btn_down.png')
 
     def go_to_main_menu(self):
         app = App.get_running_app()
