2604bafd852b15e4138858cc99318a51d0cf227b
IvanKorolev <purpleguy15122005@gmail.com>
2026-01-04 19:59:04 +0300

    dMMxc

--- src/database.py
+++ src/database.py
@@ -98,10 +98,18 @@ class myDataBase:
             (4,),
             (5,)
         ]
+<<<<<<< HEAD
 
         pre_user = [
             ("admin", 12345, 0, None)
         ]
+=======
+        
+        pre_person = [
+            ("admin", "123450", 0, None)
+        ]
+
+>>>>>>> 0d57d91 (c)
         try:
             rq.init(self.con, pre_films, statuses, pre_genres, ratings, pre_user)
 
